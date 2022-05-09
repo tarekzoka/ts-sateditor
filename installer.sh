40ab46e87309f8bd5b075a8daa9a4d7d23f816ea
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-10 00:31:42 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tar1971/feeds-finder/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tarekzoka/ts-sateditor/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tarekzoka/ts-sateditor/blob/main/enigma2-plugin-extensions-ts-sateditor_3.3all.ipk?raw=true
+
 sleep 2;
 exit 0
 MY_EM='============================================================================================================'
